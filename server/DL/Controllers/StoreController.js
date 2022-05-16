5cd49a3f8d7ebf2b8278532ac47fdb027f36f0f2
Nava Sotto <sottonava@gmail.com>
2022-05-16 12:14:31 +0300

    make it better

--- server/DL/Controllers/StoreController.js
+++ server/DL/Controllers/StoreController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const appReq = require("../../baseUrlReq");
 
 //=====================================================================
 
@@ -14,7 +15,7 @@ function sortResultsById(arr) {
 async function read(reqDetails) {
   const { category, page, per_page, order, sortById } = reqDetails;
   console.log(order);
-  return await axios.get(`https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&q=${category}&page=${page}&per_page=${per_page}&order=${order}&pretty=true`)
+  return await axios.get(`${appReq.baseUrl} +https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&q=${category}&page=${page}&per_page=${per_page}&order=${order}&pretty=true`)
     .then(res => {
       const totalHits = parseInt(res.data.totalHits);
       var currentPageImgArr = res.data.hits.map(({ id, webformatURL }) => ({ "id": id, "src": webformatURL }));
@@ -32,7 +33,7 @@ async function read(reqDetails) {
 
 async function readOne(id) {
   console.log("in readone in store controller " + id);
-  return await axios.get(`https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&id=${id}`)
+  return await axios.get(`${appReq.baseUrl} + https://pixabay.com/api/?key=25540812-faf2b76d586c1787d2dd02736&id=${id}`)
     .then(res => {
       const result = res.data.hits[0];
       return result;
---
+++ server/baseUrlReq.js
@@ -0,0 +1,15 @@
+const port = process.env.PORT || '8080';
+
+const appReq = axios.create({
+  baseURL: 'https://my-store2022.herokuapp.com:' + port,
+  withCredentials: false, // This is the default
+  crossDomain: true,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  timeout: 10000
+});
+
+console.log('PORT:', 'https://timesheet-cloud.herokuapp.com:' + port); /* eslint-disable-line no-console */
+export default appReq;
